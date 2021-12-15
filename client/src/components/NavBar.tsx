1a85ee1256a658b946d0e04f6bf117597aae7d7b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-12-15 12:50:55 +0200

    Create NavBar.tsx

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>History</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
